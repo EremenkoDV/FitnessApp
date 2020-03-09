27b55102fa39c260fe004f60ab672fe33ef642ff
d1550er@gmail.com <d1550er@gmail.com>
2020-03-09 17:27:24 +0300

    Created user model

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Gender
+    /// </summary>
+    public class Gender
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="name">Gender</param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    public class User
+    {
+
+        #region Properties
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// User gender
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// User birthdate
+        /// </summary>
+        public DateTime Birthday { get; }
+
+        /// <summary>
+        /// User weight
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// User height
+        /// </summary>
+        public double Height { get; set; }
+
+        #endregion Properties
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">user's name</param>
+        /// <param name="gender">user's gender</param>
+        /// <param name="birthday">user's birthday</param>
+        /// <param name="weight">user's weight</param>
+        /// <param name="height">user's height</param>
+        public User(string name,
+                    Gender gender,
+                    DateTime birthday,
+                    double weight,
+                    double height)
+        {
+            #region Arguments validation
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя не может быть пустым или null.", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть пустым.", nameof(gender));
+            }
+
+            if (birthday < DateTime.Parse("01.01.1900") || birthday >= DateTime.Now)
+            {
+                throw new ArgumentException("Дата рождения должна быть датой, не меньше 01.01.1900 и не больше текущей.", nameof(birthday));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Вес должен быть числом, больше 0.", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост должен быть числом, больше 0.", nameof(height));
+            }
+
+            #endregion Arguments validation
+
+            Name = name;
+            Birthday = birthday;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
