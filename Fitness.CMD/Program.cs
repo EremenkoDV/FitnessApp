ad0d88c893a463d38bde02669a47dc80a334c0c6
d1550er@gmail.com <d1550er@gmail.com>
2020-03-12 17:51:01 +0300

    Added TestConditions method (under constructions)

--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -3,6 +3,7 @@ using Fitness.BL.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace Fitness.CMD
         static void Main(string[] args)
         {
             Console.WriteLine("Вас приветствует приложение Fitness.");
-            int age = GetInputedValue<int>("возраст пользователя: ");
+            int age = GetInputedValue<int>("возраст пользователя: ", "value > 0");
             DateTime birthday = GetInputedValue<DateTime>("день рождения пользователя: ");
             double weight = GetInputedValue<double>("вес пользователя: ");
             double height = GetInputedValue<double>("рост пользователя: ");
@@ -27,7 +28,6 @@ namespace Fitness.CMD
             {
                 Console.Write("Введите пол пользователя:");
                 string gender = Console.ReadLine();
-
                 //DateTime birthday = GetInputedValue<DateTime>("день рождения пользователя: ");
                 //double weight = GetInputedValue<double>("вес пользователя: ");
                 //double height = GetInputedValue<double>("рост пользователя: ");
@@ -55,7 +55,7 @@ namespace Fitness.CMD
             }
         }
 
-        public static T GetInputedValue<T>(string parameter, string conditions)
+        public static T GetInputedValue<T>(string parameter, string conditions = "")
         {
             int attemps = 3;
             T result;
@@ -65,7 +65,14 @@ namespace Fitness.CMD
 
                 if (TryParse<T>(Console.ReadLine(), out result))
                 {
-                    return result;
+                    if (TestCondition(result, conditions))
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Введены данные не соответствующие условию : {conditions}. {(attemps == 1 ? "Осталась " + attemps + " попытка" : "Осталось " + attemps + (attemps > 1 && attemps < 5 ? " попытки" : " попыток"))}.");
+                    }
                 }
                 else if (attemps-- > 0)
                 {
@@ -77,6 +84,20 @@ namespace Fitness.CMD
             Console.WriteLine("Количество попыток исчерпано!");
             throw new ArgumentException("Введены некоректные данные. Количество попыток исчерпано!", nameof(parameter));
 
+        }
+
+        public static bool TestCondition<T>(T value, string c)
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("value", typeof(T));
+            DataRow row = dt.NewRow();
+            //dt.Rows.Add("value");
+            row["value"] = value;
+            // hack
+            // c is constant and doesn't depend on dt columns
+            // if there are any rows, c is TRUE
+            var rows = dt.Select(c);
+            return rows.Length > 0;
         }
 
     }
